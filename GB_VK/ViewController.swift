38695636af82ff4a898aed0cf2b9f85b458a8e34
Zen13 <zen-job@mail.ru>
2017-09-14 21:48:36 +0300

    Initial Commit

---
+++ GB_VK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GB_VK
+//
+//  Created by Zen on 14.09.17.
+//  Copyright © 2017 Zen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
